0cd32fb52995b692fc1eb06571d59a291f8b96fc
Julio Henrique <tecjuliohenrique@gmail.com>
2018-05-20 23:31:03 -0300

    diretivas, pipes

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { FormatCurrencyPipe } from './format-currency.pipe';
+import { FormatDatePipe } from './format-date.pipe';
+import { MyTaskDirective } from './my-task.directive';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskListComponent,
+    FormatCurrencyPipe,
+    FormatDatePipe,
+    MyTaskDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
